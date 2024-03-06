4484157633a3cd085b159a4404065cd5b28fa11e
guedir <guedirmounir@gmail.com>
2024-03-06 12:12:43 +0100

    première mise à jour

--- mini_projet1.py
+++ mini_projet1.py
@@ -1,19 +1,39 @@
 import tkinter
+from tkinter import messagebox
 
-#Fenetre de connection
+def SeConnecter():
+    champ1 = zone_saisi1.get()
+    champ2 = zone_saisi2.get()
+    if (champ1 == "") and (champ2 == ""):
+        messagebox.showerror("Erreur","veuillez remplire les champs")
+        zone_saisi1.delete("0","end")
+        zone_saisi2.delete("0","end")
+    elif champ1 == "admin" and champ2 == "0000":
+        messagebox.showinfo("connecté","bienvenue")
+        zone_saisi1.delete("0","end")
+        zone_saisi2.delete("0","end")
+        fenetre.destroy()
+    else:
+        messagebox.showwarning("Attention" , "Nom utilisateur ou Mot de passe incorrecte")
+        zone_saisi1.delete("0","end")
+        zone_saisi2.delete("0","end")
+    
+#Fenetre de connexion
 fenetre = tkinter.Tk()
-fenetre.title("FENETRE DE CONNECTION")
+fenetre.title("FENETRE DE CONNEXION")
 fenetre.resizable(False,False)
 fenetre.geometry("300x200")
+fenetre.configure(bg="#091821")
 
 #Formulaire
-titre = tkinter.Label(text="Formulaire de connexion")
-user = tkinter.Label(text="Nom Utilisateur :")
-zone_saisi1 = tkinter.Entry()
-mp = tkinter.Label(text="Mot de Passe :")
-zone_saisi2 = tkinter.Entry()
-btn = tkinter.Button(text="Connexion")
+titre = tkinter.Label(fenetre , text="Formulaire de connexion" , bg="#000000" , fg="#FFFFFF" , font=("sans serif" , 18))
+user = tkinter.Label(fenetre , text="Nom Utilisateur :" ,  bg="#091821" , fg="#FFFFFF" , font=("",10,"bold"))
+zone_saisi1 = tkinter.Entry(fenetre , bd=2)
+mp = tkinter.Label(fenetre , text="Mot de Passe :" , bg="#091821" , fg="#FFFFFF", font=("",10,"bold"))
+zone_saisi2 = tkinter.Entry(fenetre , show="*" , bd=2)
+btn = tkinter.Button(fenetre,text="Connexion",bg="#FF4500",fg="white",font=("Arial",10,"bold"),bd=3,command=SeConnecter)
 
+#Placement des Widgets
 titre.place(x=0 , y=0 , width=300)
 user.place(x=10 , y=60 )
 zone_saisi1.place(x=120 , y=60 , width=150)
