d937513e174117adb6a48c569e0fb0aaa6705c53
guedir <guedirmounir@gmail.com>
2024-03-09 16:29:00 +0100

    Amélioration des boutton et ajout du combobox et de la table

--- Interface_de_gestion.py
+++ Interface_de_gestion.py
@@ -1,4 +1,5 @@
 import tkinter
+from tkinter import ttk
 
 #FENETRE PRINCIPALE
 page = tkinter.Tk()
@@ -23,29 +24,31 @@ saisi_matricule.place(x=250 , y=150 , width=150)
 #LABEL NOM
 lbl_nom = tkinter.Label(page, text="NOM" , font=("arial",15) , bg="#091821" , fg="#FFFFFF")
 saisi_nom = tkinter.Entry(page, bd=4, font=("Arial" , 15))
-lbl_nom.place(x=70 , y=200 )
+lbl_nom.place(x=70 , y=200  )
 saisi_nom.place(x=250 , y=200 )
 
 #LABEL PRENOM
 lbl_prenom = tkinter.Label(page, text="PRENOM" , font=("arial",15) , bg="#091821" , fg="#FFFFFF")
 saisi_prenom = tkinter.Entry(page, bd=4, font=("Arial" , 15))
-lbl_prenom.place(x=70 , y=250)
+lbl_prenom.place(x=70 , y=250 )
 saisi_prenom.place(x=250 , y=250)
 
 #SEXE ELEVE
 valeursexe= tkinter.StringVar()
 sexe_masculin = tkinter.Radiobutton(page, text="MASCULIN", bg="#091821", fg="#696969",font=("arial",14), value="M",
-                                     indicatoron=0 )
+                                    variable=valeursexe, indicatoron=0 )
 sexe_feminin = tkinter.Radiobutton(page , text="FEMININ", bg="#091821", fg="#696969", font=("arial",14), value="F",
-                                    indicatoron=0)
+                                    variable=valeursexe, indicatoron=0)
 sexe_masculin.place(x=250 , y=300, width=100)
 sexe_feminin.place(x=380 , y=300, width=100)
 
 #LABEL CLASSE
 lbl_classe = tkinter.Label(page, text="CLASSE" , font=("arial",15) , bg="#091821" , fg="#FFFFFF")
-saisi_classe = tkinter.Entry(page, bd=4, font=("Arial" , 15))
 lbl_classe.place(x=70 , y=350 )
-saisi_classe.place(x=250 , y=350 )
+option = ["CP" , "CE1" , "CE2" , "CM1" , "CM2" , "6e" , "5e" , "4e" , "3e"]
+option_selected = tkinter.StringVar()
+combo = ttk.Combobox(page, font=("Arial" , 14), textvariable=option_selected, values=option)
+combo.place(x=250 , y=350 , width=100 )
 
 #LABEL MATIERE
 lbl_matiere = tkinter.Label(page, text="MATIERE" , font=("arial",15) , bg="#091821" , fg="#FFFFFF")
@@ -60,11 +63,32 @@ lbl_note.place(x=70 , y=450)
 saisi_note.place(x=250 , y=450, width=120)
 
 #BOUTTONS
-btn_enregistrer = tkinter.Button(page , text="Eregistrer")
-btn_modifier = tkinter.Button(page , text="Modifier")
-btn_supprimer = tkinter.Button(page , text="Supprimer")
+btn_enregistrer = tkinter.Button(page , text="Eregistrer" , bg="#FF4500" , fg="#FFFFFF", font=("Arial",10,"bold"), bd=4)
+btn_modifier = tkinter.Button(page , text="Modifier", bg="#FF4500" , fg="#FFFFFF", font=("Arial",10,"bold"), bd=4)
+btn_supprimer = tkinter.Button(page , text="Supprimer", bg="#FF4500" , fg="#FFFFFF", font=("Arial",10,"bold"), bd=4)
 btn_enregistrer.place(x=250 , y=500, width=120)
 btn_modifier.place(x=250 , y=550, width=120)
 btn_supprimer.place(x=250 , y=600, width=120)
+
+#TABLE
+table = ttk.Treeview(page , columns=(1,2,3,4,5,6,7), show="headings", height=5)
+table.place(x=520 , y=150 , width=780 ,height=500)
+
+#ENTETE
+table.heading(1 , text="MAT")
+table.heading(2 , text="NOM")
+table.heading(3 , text="PRENOM")
+table.heading(4 , text="SEXE")
+table.heading(5 , text="CLASSE")
+table.heading(6 , text="MATIERE")
+table.heading(7 , text="NOTE")
+
+table.column(1, width=30)
+table.column(2, width=100)
+table.column(3, width=100)
+table.column(4, width=30)
+table.column(5, width=30)
+table.column(6, width=100)
+table.column(7, width=30)
 
 page.mainloop()
